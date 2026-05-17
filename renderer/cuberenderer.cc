69b0f31868f262eb10254f9cfb31eeeacda8dbbf
s4326202 <s4326202@vuw.leidenuniv.nl>
2026-05-17 12:04:51 +0200

    upd: understand renderer now

--- renderer/cuberenderer.cc
+++ renderer/cuberenderer.cc
@@ -1,12 +1,26 @@
 #include "include/cuberenderer.h"
 
 CubeRenderer::CubeRenderer(RCube *c, GLuint shader) : cube(c) {
+  //create viewing vector space
+  //using three vectors as it is a 3d space
+  //eye : location of camera 
+  //center : the location where the camera is looking
+  //up : defines the Y-axis (0, 1, 0) is default up to positive y
+    //basically which way is up in respect to (0, 0, 0)
+  //the X-axis and Z-axis are calculated based on eye and center
   view = glm::lookAt(
     glm::vec3(6, 6, 6),
     glm::vec3(0, 0, 0),
     glm::vec3(0, 1, 0)
   );
 
+  //create a projection matrix so that we can see depth
+  //4 arguments
+  //FOV : how wide the camera lens is in radians (this case 45 degrees)
+  //aspect ratio : width / height (this case just 1 to 1)
+  //near clipping plane : anything closer than this distance to the camera
+    //will not be rendered to prevent clipping
+  //far clipping plane : maximum draw distance (any object further away is ignored)
   projection = glm::perspective(
     glm::radians(45.0f),
     1.0f,
@@ -14,6 +28,9 @@ CubeRenderer::CubeRenderer(RCube *c, GLuint shader) : cube(c) {
     100.0f
   );
   shaderProgram = shader;
+
+  //grab location of all uniform variables in the shader program
+  //uniform variables are practically global variables that you can change
   modelLoc = glGetUniformLocation(shaderProgram, "model");
   viewLoc = glGetUniformLocation(shaderProgram, "view");
   projLoc = glGetUniformLocation(shaderProgram, "projection");
@@ -22,8 +39,8 @@ CubeRenderer::CubeRenderer(RCube *c, GLuint shader) : cube(c) {
 }
 
 void CubeRenderer::initMesh() {
+  //all vertex positions 
   float vertices[] = {
-    // positions
     -0.5f, -0.5f, -0.5f,
     0.5f, -0.5f, -0.5f,
     0.5f,  0.5f, -0.5f,
@@ -34,6 +51,7 @@ void CubeRenderer::initMesh() {
     -0.5f,  0.5f,  0.5f
   };
 
+  //all vertex indices used to form triangles 
   unsigned int indices[] = {
     0,1,2, 2,3,0,
     4,5,6, 6,7,4,
@@ -43,48 +61,88 @@ void CubeRenderer::initMesh() {
     0,1,5, 5,4,0
   };
 
+  //generate VAO, VBO and EBO
   glGenVertexArrays(1, &cubeVAO);
   glGenBuffers(1, &cubeVBO);
   glGenBuffers(1, &cubeEBO);
 
+  //bind all future vertex array function calls to this VAO
+  //bind all future buffer function calls related to ELEMENT_ARRAY_BUFFER to EBO
+  //bind all future buffer function calls related to ARRAY_BUFFER to VBO
   glBindVertexArray(cubeVAO);
-
-  glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeEBO);
+  glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+
+  //put data in ARRAY BUFFER and ELEMENT ARRAY BUFFER 
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-  // position attribute (location = 0 in shader)
+  //what data means what in the VBO is specified here
+  //basically in the shader program we created a location = 0 (we can create multiple)
+  //these locations contain data for every attribute of the vertices
+  //in this case we only have position so we only need one
+  //arguments: 
+  //index : the location specified in you .vert shader program
+  //size : number of data points per vertex attribute 
+  //type : datatype of the array elements
+  //normalized : basically if you want fixed point values or just take exact values
+    //GL_TRUE = fixed
+    //GL_FALSE = raw data
+  //stride : byte offset between consecutive attributes
+  //offset : where to start reading in the array
   glVertexAttribPointer(
-      0, 3, GL_FLOAT, GL_FALSE,
-      3 * sizeof(float),
-      (void*)0
+    0, 3, GL_FLOAT, GL_FALSE,
+    3 * sizeof(float),
+    (void*)0
   );
 
+  //allow the use of the VBO during rendering
+  //pass the attribute location value as specified in the shader program
   glEnableVertexAttribArray(0);
 
+  //unbind the VAO so we can't accidentally change it later
   glBindVertexArray(0);
-}
+} //initMesh
 
 void CubeRenderer::render() {
+  //define which shader program to use
   glUseProgram(shaderProgram);
 
+  //grab single cubit
   RenderCubit& c = cubits[1][1][1];
 
+  //create identity matrix
   glm::mat4 model(1.0f);
 
+  //multiply model by a vector
   model = glm::translate(
     model,
     glm::vec3(0.0f, 0.0f, 0.0f)
   );
 
+  //multiply model by orientation
   model *= c.orientation;
 
+  //set all global variables in the shader
   glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &model[0][0]);
   glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
   glUniformMatrix4fv(projLoc, 1, GL_FALSE, &projection[0][0]);
 
+  //set used vertex array to VAO
   glBindVertexArray(cubeVAO);
+  //draw all elements
+  //mode: TRIANGLES
+  //count: amount of elements
+  //type: the data type of the values in indices
+  //indices: a byte offset into the buffer bound to draw from
   glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
+  //unbind VAO
+  glBindVertexArray(0);
+}
+
+CubeRenderer::~CubeRenderer() {
+  //destroy all gl buffers and arrays
+  glDeleteBuffers(1, &cubeVBO);
+  glDeleteBuffers(1, &cubeEBO);
+  glDeleteVertexArrays(1, &cubeVAO);
 }
--- renderer/include/cuberenderer.h
+++ renderer/include/cuberenderer.h
@@ -14,11 +14,14 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+//datastructure to hold orientation of cubit
 struct RenderCubit {
-  //identity matrix
+  //holds the orientation of the cubit 
+  //starts on identity matrix
   glm::mat4 orientation = glm::mat4(1.0f);
 };
 
+//datastructure to hold all data for the animation movement of a side
 struct MoveAnimation {
   bool active = false;
 
@@ -33,36 +36,73 @@ struct MoveAnimation {
   std::vector<int> cubits;
 };
 
+//main class of the cube renderer 
+//containing both the shader and all cubits and the main logical cube
 class CubeRenderer {
   private: 
+    //pseudo logic cube to keep track of cubit positions
     RenderCubit cubits[MAX_D][MAX_D][MAX_D];
     
+    //animation stack
     MoveAnimation animation;
 
+    //main logical cube
     RCube *cube = nullptr;
 
+    //main shader program to be used
     GLuint shaderProgram;
 
+    //(vertex array object)
+    //contains data to know how to reaed the VBO correctly,
+    //like the first 3 floats in the VBO are positional
+    //then there is 2 floats for texture coordinates etc... 
+    //like a recipe to use when reading the VBO for the GPU
     GLuint cubeVAO;
+    //(vertex buffer object)
+    //contains all raw data like 
+    //positions, colours and textures for every vertex
+    //and stores it in the GPU
+    //it practically holds any per-vertex data
+    //but the GPU has no idea how to parse this thing
+    //as it needs to know what data is where and cuz its dumb
+    //thats why we make VAO
     GLuint cubeVBO;
+    //(element buffer object)
+    //this is a memory saving buffer that contains information
+    //on how to connect vertices to form shapes for the GPU
+    //holds an array of integers that act as pointers to the vertices in the VBO
+    //and it practically tells the GPU stuff like use vertices 1, 3, and 5 to draw a triangle
+    //with an EBO you can now use glDrawElements instead of glDrawArrays
     GLuint cubeEBO;
 
+    //number of elements to be drawn
     int indexCount = 36;
 
+    //global variable locations in shader program
     GLint modelLoc;
     GLint viewLoc;
     GLint projLoc;
 
+    //main viewing matrix
     glm::mat4 view;
+    //3d projection matrix
     glm::mat4 projection;
 
   public: 
+    //default constructor
     CubeRenderer(RCube *c, GLuint shader);
 
+    //default destructor
+    ~CubeRenderer();
+
+    //initialise the mesh 
     void initMesh();
 
+    //update the animation
     void update(float deltaTime){};
+    //render the cube
     void render();
+    //initiate an animation
     void startMove(char side, Rotation direction){};
 };
 #endif
